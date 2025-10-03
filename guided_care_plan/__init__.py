061dd0e78d76fff577ceccf2e19922a9f5828d04
shaneb74 <shaneb@conciergecareadvisors.com>
2025-10-03 14:00:04 -0500

    Update __init__.py

--- guided_care_plan/__init__.py
+++ guided_care_plan/__init__.py
@@ -3,6 +3,11 @@
 from .state import ensure_gcp_session, render_stepper, get_question_meta, QUESTION_ORDER
 from .engine import evaluate_guided_care
 
+from ui.theme import inject_theme
+
+def ensure_gcp_theme():
+    inject_theme()
+
 __all__ = [
     "ensure_gcp_session",
     "render_stepper",
