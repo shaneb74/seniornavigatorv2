4143c14951ef79e30f5d66864beb55918d671acb
shaneb74 <shaneb@conciergecareadvisors.com>
2025-10-06 11:56:40 -0500

    Update SeniorNav_welcome_self.py

--- app_pages/SeniorNav_welcome_self.py
+++ app_pages/SeniorNav_welcome_self.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 import streamlit as st
-st.set_page_config(layout='wide', page_title='Welcome · For You')
-from pages.SeniorNav_welcome_base import render
+
+from app_pages.SeniorNav_welcome_base import render
 render("self")
