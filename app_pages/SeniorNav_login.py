64aedf84c00c2b88aa933c21348a5d11f2caf490
shaneb74 <shaneb@conciergecareadvisors.com>
2025-10-06 11:51:49 -0500

    Update SeniorNav_login.py

--- app_pages/SeniorNav_login.py
+++ app_pages/SeniorNav_login.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import streamlit as st
 from ui.theme import inject_theme
-from pages.seniornav_util import top_nav, safe_switch
+from app_pages.seniornav_util import top_nav, safe_switch
 
 st.set_page_config(layout="wide", page_title="Sign in")
 inject_theme()
